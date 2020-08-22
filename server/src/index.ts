cd86ebf52eaac4fea7f42df970739a48d65779d2
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-08-22 11:34:00 +0530

    Add crud

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata';
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-//import { Post } from "./entities/Post";
+import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -16,10 +18,13 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [],
+      resolvers: [PostResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log("server running @ port 4000");
